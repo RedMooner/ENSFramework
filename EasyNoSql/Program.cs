d7a9cd4be1bb6640d98f6dd443c713dbc19248ec
RedMoon <SashaM122@mail.ru>
2023-05-18 10:49:21 +0500

    added methods for migrating entities to the database, work is being done on configuring SQL connections

--- EasyNoSql/Program.cs
+++ EasyNoSql/Program.cs
@@ -1,18 +1,55 @@
-﻿using System.Reflection;
+﻿using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace ENS
 {
     public class DataBase
     {
-        public string Name { get; set; }
-        public DataBase()
-        {
-            Name = "somedb";
-        }
-        public void MakeMigrations()
-        {
+        #region Properties
+        public string Name { get; private set; }
+        public int Port { get; private set; }
+        public string Server { get; private set; }
+        private string Password { get; set; }
+        private string Username { get; set; }
 
+        private MySqlConnection _connection;
+        public DataBase(string name, int port, string server, string password, string username)
+        {
+            Name = name;
+            Port = port;
+            Server = server;
+            Password = password;
+            Username = username;
+            CreateConnection();
         }
+
+        #endregion
+        #region SQLworkMethods
+        private void CreateConnection()
+        {
+            try
+            {
+                _connection = new MySqlConnection($"database={Name},server={Server},port={Port},username={Username},pwd={Password}");
+                _connection.Open();
+                _connection.Close();
+            }
+            catch
+            {
+                Debug.WriteLine("Connection creation error, please check logs!");
+            }
+        }
+        private bool CheckExistence()
+        {
+            return false;
+        }
+        private MySqlDataReader Query(string sql)
+        {
+            MySqlCommand command = new MySqlCommand(sql,_connection);
+            return command.ExecuteReader();
+        }
+        #endregion
+        #region Migrations
         public void Migrate()
         {
             var instances = from t in Assembly.GetExecutingAssembly().GetTypes()
@@ -31,6 +68,7 @@ namespace ENS
                 Console.WriteLine("Done!");
             }
         }
+        #endregion
     }
 
     public interface ITable
@@ -45,7 +83,7 @@ namespace ENS
         public string Name { get; set; }
         public string Password { get; set; }
     }
-    public class Orders  : ITable
+    public class Orders : ITable
     {
         public int Id { get; set; }
         public int User { get; set; }
@@ -56,7 +94,7 @@ namespace ENS
     {
         public static void Main(string[] args)
         {
-            DataBase dataBase = new DataBase();
+            DataBase dataBase = new DataBase("test", 3307, "localhost", "", "root");
             dataBase.Migrate();
             Console.Read();
         }
